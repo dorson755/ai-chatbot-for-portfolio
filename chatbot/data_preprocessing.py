5132d750e9faa35e1ce4fc858076b2bf648c438b
Dorson Williams <68614535+dorson755@users.noreply.github.com>
2024-09-22 02:02:03 +0000

    edited data_preprocessing to accomodate the tsv file extensions.

--- chatbot/data_preprocessing.py
+++ chatbot/data_preprocessing.py
@@ -1,16 +1,33 @@
 import pandas as pd
 
-# Load movie lines
-lines_file = './data/cornell_movie_dialogs_corpus/movie_lines.txt'
-conversations_file = './data/cornell_movie_dialogs_corpus/movie_conversations.txt'
+# Load movie lines and conversations
+lines_file = './data/cornell_movie_dialogs_corpus/movie_lines.tsv'
+conversations_file = './data/cornell_movie_dialogs_corpus/movie_conversations.tsv'
 
-# Read lines
-lines = pd.read_csv(lines_file, sep='+++$+++', header=None, names=['line_id', 'character', 'movie', 'text'], engine='python')
-lines['line_id'] = lines['line_id'].str.strip()
+# Read lines (using the correct separator for TSV)
+lines = pd.read_csv(lines_file, sep='\t', header=None, names=['line_id', 'character', 'movie', 'text'])
 
 # Read conversations
-conversations = pd.read_csv(conversations_file, sep='+++$+++', header=None, names=['character1', 'character2', 'lines'], engine='python')
+conversations = pd.read_csv(conversations_file, sep='\t', header=None, names=['character1', 'character2', 'lines'])
 
 # Display the first few rows
 print(lines.head())
 print(conversations.head())
+
+# Create a list to hold questions and answers
+questions = []
+answers = []
+
+# Extracting the conversations
+for index, row in conversations.iterrows():
+    line_ids = row['lines'].strip().split(' ')
+    
+    for i in range(len(line_ids) - 1):
+        questions.append(lines.loc[lines['line_id'] == line_ids[i], 'text'].values[0])
+        answers.append(lines.loc[lines['line_id'] == line_ids[i + 1], 'text'].values[0])
+
+# Create a DataFrame for the questions and answers
+qa_pairs = pd.DataFrame({'question': questions, 'answer': answers})
+
+# Display the first few question-answer pairs
+print(qa_pairs.head())
